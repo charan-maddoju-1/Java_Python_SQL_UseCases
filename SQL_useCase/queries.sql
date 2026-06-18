0a078aa1ddca287c04c385e938743b524f4f32bb
charan_maddoju <charanmaddoju1@gmail.com>
2026-06-18 08:16:28 +0530

    Added some more sample data into db

--- SQL_useCase/queries.sql
+++ SQL_useCase/queries.sql
@@ -1,3 +1,9 @@
+SELECT * from Partners;
+SELECT * from Shipments;
+SELECT * from DeliveryLogs;
+
+
+
 --Delayed Shipments
 SELECT ShipmentID, PartnerID, PromisedDate, ActualDeliveryDate
 FROM Shipments
@@ -7,11 +13,12 @@ WHERE ActualDeliveryDate > PromisedDate;
 --Performance Ranking
 SELECT p.PartnerName,
     COUNT(CASE WHEN s.Status = 'Delivered' THEN 1 END) AS SuccessfulDeliveries,
-    COUNT(CASE WHEN s.Status = 'Returned' THEN 1 END) AS ReturnedDeliveries
+    COUNT(CASE WHEN s.Status = 'Returned' THEN 1 END) AS ReturnedDeliveries,
+    ROUND(100.0 * COUNT(CASE WHEN s.Status = 'Delivered' THEN 1 END) /COUNT(*),2) AS SuccessRate
 FROM Partners p
 JOIN Shipments s ON p.PartnerID = s.PartnerID
 GROUP BY p.PartnerName
-ORDER BY SuccessfulDeliveries-ReturnedDeliveries DESC;
+ORDER BY SuccessRate DESC ,SuccessfulDeliveries-ReturnedDeliveries DESC;
 
 
 --Most Popular Destination (Last 30 Days)
@@ -24,20 +31,15 @@ LIMIT 1;
 
 
 --Partner Scorecard
-SELECT 
-    p.PartnerName,
+SELECT p.PartnerName,
     COUNT(s.ShipmentID) AS TotalShipments,
-
     COUNT(CASE 
         WHEN s.ActualDeliveryDate > s.PromisedDate THEN 1 
     END) AS DelayedShipments,
-
-    ROUND(
-        COUNT(CASE WHEN s.Status = 'Delivered' THEN 1 END) * 100.0 
-        / COUNT(s.ShipmentID), 2
-    ) AS SuccessRate
-
+    ROUND( COUNT(CASE WHEN s.Status = 'Delivered' THEN 1 END) * 100.0 / COUNT(s.ShipmentID), 2) AS SuccessRate
 FROM Partners p
 JOIN Shipments s ON p.PartnerID = s.PartnerID
 GROUP BY p.PartnerName
 ORDER BY DelayedShipments ASC;
+
+
--- SQL_useCase/smapleData.sql
+++ SQL_useCase/smapleData.sql
@@ -1,7 +1,8 @@
 --Partners
-INSERT INTO Partners VALUES (1, 'FastExpress');
-INSERT INTO Partners VALUES (2, 'QuickShip');
-INSERT INTO Partners VALUES (3, 'SpeedX');
+INSERT INTO Partners VALUES 
+(1, 'FastExpress'),
+(2, 'QuickShip'),
+(3, 'SpeedX');
 
 --Shipments
 INSERT INTO Shipments VALUES
@@ -17,3 +18,64 @@ INSERT INTO DeliveryLogs VALUES
 (1, 101, 'Picked Up', DATETIME('now','-5 days')),
 (2, 101, 'In Transit', DATETIME('now','-3 days')),
 (3, 101, 'Delivered', DATETIME('now','-1 days'));
+
+
+
+INSERT INTO Partners VALUES
+(4, 'BlueDart'),
+(5, 'SafeCargo'),
+(6, 'RapidMove');
+
+INSERT INTO Shipments VALUES
+(107, 4, 'Mumbai', 'Kolkata', DATE('now','-8 days'), DATE('now','-4 days'), DATE('now','-3 days'), 'Delivered'),
+(108, 4, 'Pune', 'Hyderabad', DATE('now','-5 days'), DATE('now','-2 days'), NULL, 'In Transit'),
+(109, 5, 'Chennai', 'Delhi', DATE('now','-12 days'), DATE('now','-7 days'), DATE('now','-5 days'), 'Delivered'),
+(110, 5, 'Bangalore', 'Mumbai', DATE('now','-6 days'), DATE('now','-3 days'), DATE('now','-2 days'), 'Returned'),
+(111, 6, 'Kolkata', 'Pune', DATE('now','-4 days'), DATE('now','-1 days'), NULL, 'In Transit'),
+(112, 6, 'Hyderabad', 'Delhi', DATE('now','-9 days'), DATE('now','-5 days'), DATE('now','-4 days'), 'Delivered'),
+(113, 1, 'Mumbai', 'Chennai', DATE('now','-7 days'), DATE('now','-3 days'), DATE('now','-2 days'), 'Delivered'),
+(114, 2, 'Delhi', 'Hyderabad', DATE('now','-5 days'), DATE('now','-2 days'), NULL, 'In Transit'),
+(115, 3, 'Pune', 'Bangalore', DATE('now','-10 days'), DATE('now','-6 days'), DATE('now','-4 days'), 'Delivered');
+
+INSERT INTO DeliveryLogs VALUES
+(4, 107, 'Picked Up', DATETIME('now','-8 days')),
+(5, 107, 'In Transit', DATETIME('now','-5 days')),
+(6, 107, 'Delivered', DATETIME('now','-3 days')),
+
+(7, 108, 'Picked Up', DATETIME('now','-5 days')),
+(8, 108, 'In Transit', DATETIME('now','-2 days')),
+
+(9, 109, 'Picked Up', DATETIME('now','-12 days')),
+(10, 109, 'In Transit', DATETIME('now','-8 days')),
+(11, 109, 'Delivered', DATETIME('now','-5 days')),
+
+(12, 110, 'Picked Up', DATETIME('now','-6 days')),
+(13, 110, 'In Transit', DATETIME('now','-4 days')),
+(14, 110, 'Returned', DATETIME('now','-2 days')),
+
+(15, 111, 'Picked Up', DATETIME('now','-4 days')),
+(16, 111, 'In Transit', DATETIME('now','-1 days')),
+
+(17, 112, 'Picked Up', DATETIME('now','-9 days')),
+(18, 112, 'In Transit', DATETIME('now','-6 days')),
+(19, 112, 'Delivered', DATETIME('now','-4 days')),
+
+(20, 113, 'Picked Up', DATETIME('now','-7 days')),
+(21, 113, 'In Transit', DATETIME('now','-4 days')),
+(22, 113, 'Delivered', DATETIME('now','-2 days')),
+
+(23, 114, 'Picked Up', DATETIME('now','-5 days')),
+(24, 114, 'In Transit', DATETIME('now','-2 days')),
+
+(25, 115, 'Picked Up', DATETIME('now','-10 days')),
+(26, 115, 'In Transit', DATETIME('now','-7 days')),
+(27, 115, 'Delivered', DATETIME('now','-4 days'));
+
+
+INSERT INTO Shipments VALUES
+(116, 1, 'Chennai', 'Kolkata', DATE('now','-4 days'), DATE('now','-2 days'), DATE('now','-3 days'), 'Delivered');
+
+INSERT INTO DeliveryLogs VALUES
+(28, 116, 'Picked Up', DATETIME('now','-4 days')),
+(29, 116, 'In Transit', DATETIME('now','-4 days')),
+(30, 116, 'Delivered', DATETIME('now','-3 days'));
